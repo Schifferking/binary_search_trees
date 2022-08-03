0af0a0686ac752d33b9301d54e9f45921021ed15
Schifferking <schifferking@hotmail.com>
2022-08-03 12:09:45 -0500

    Add build_tree and pretty_print methods
    Changed the name of left_chil and right_child variables to left and
    right.
    
    The build_tree method consists in buil a BST using an array. The steps
    are:
    
    1. Return nil if the array is empty
    2. Remove duplicates from array if there are
    3. Sort array if it's unsorted
    4. get the index of the middle element of the array
    5. Create and assing a new node using that element to a root variable
    6. Using recursion, do the same for the left and half parts of the
       array
    7. Assign root.left from calling the same method with the left part of
       the array
    8. Assign root.right from calling the same method with the right part of
       the array
    9. Return root
    
    Finally, added a method shared on The Odin Project to pretty print a
    BST.

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,6 +1,6 @@
 class Node
   include Comparable
-  attr_accessor :data, :left_child, :right_child
+  attr_accessor :data, :left, :right
 
   def <=>(other)
     data <=> other.data
@@ -8,7 +8,38 @@ class Node
 
   def initialize(data)
     @data = data
-    @left_child = nil
-    @right_child = nil
+    @left = nil
+    @right = nil
+  end
+end
+
+class Tree
+  attr_accessor :root
+
+  def initialize(numbers)
+    @root = build_tree(numbers)
+  end
+
+  def build_tree(data)
+    return nil if data.empty?
+
+    data.uniq! unless data == data.uniq
+
+    data.sort! unless data == data.sort
+
+    middle = data.size / 2
+    root = Node.new data[middle]
+
+    root.left = build_tree(data[0, middle])
+
+    root.right = build_tree(data[middle + 1..-1])
+
+    root
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
